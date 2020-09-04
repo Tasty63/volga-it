e4b77732d172314e1a2deb9933f00909bda3ab50
Tasty <sapegin.petya@yandex.ru>
2020-09-04 23:11:25 +0400

    0.4

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const logo = document.querySelectorAll('.header__logo-top path');
+const hamburger = document.querySelector('.header__menu-icon');
+const menu = document.querySelector('header__menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
