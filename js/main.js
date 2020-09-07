3edfe8fddebf2a092eb8b3e868c55c68649809c3
Tasty <sapegin.petya@yandex.ru>
2020-09-08 00:17:21 +0400

    0.7

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const logo = document.querySelectorAll('.header__logo-top path');
 const hamburger = document.querySelector('.header__menu-icon');
-const menu = document.querySelector('.header__pop-up');
+const popUp = document.querySelector('.header__pop-up');
+const navItems = document.querySelectorAll('.nav-item');
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-});
-
-
-
-
-
-window.addEventListener('wheel', event => {
-
+    navItems.forEach(item => {
+        if (hamburger.classList.contains('active')) {
+            item.classList.add('active');
+            item.classList.remove('hidden');
+            popUp.classList.add('active');
+            popUp.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+            item.classList.remove('active');
+            popUp.classList.add('hidden');
+            popUp.classList.remove('active');
+        }
+    });
 });
